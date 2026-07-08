8477cfd69ebcfe527209aaebd29ead7c4da6d29f
willardrogerd-a11y <willard.roger.d@gmail.com>
2026-07-07 22:11:45 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dchqyr...supabase.co'
+const supabaseUrl = 'https://dchqyrsupabase.co'
 const supabaseAnonKey = 'sb_publishable_Oz9JHZ0vxpQwFZtBNmZfHQ_fLIbEmOd'
 
 export const supabase = createClient(
